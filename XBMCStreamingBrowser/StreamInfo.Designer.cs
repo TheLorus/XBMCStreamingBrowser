39514b855ed9c80956ea71a5643afb124c340d3f
siriuzwhite <TheLorus@users.noreply.github.com>
2014-07-26 11:35:47 +0200

    making progress

--- XBMCStreamingBrowser/StreamInfo.Designer.cs
+++ XBMCStreamingBrowser/StreamInfo.Designer.cs
@@ -28,25 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.HosterBox = new System.Windows.Forms.ComboBox();
+            this.BoxHoster = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.MirrorBox = new System.Windows.Forms.ComboBox();
+            this.BoxMirror = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.LabelSeason = new System.Windows.Forms.Label();
             this.LabelEpisode = new System.Windows.Forms.Label();
             this.BoxSeason = new System.Windows.Forms.ComboBox();
             this.BoxEpisode = new System.Windows.Forms.ComboBox();
+            this.ButtonOk = new System.Windows.Forms.Button();
+            this.LabelPart = new System.Windows.Forms.Label();
+            this.BoxPart = new System.Windows.Forms.ComboBox();
+            this.LabelHosterLink = new System.Windows.Forms.Label();
+            this.LabelPlay = new System.Windows.Forms.Label();
+            this.ButtonPlay = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // HosterBox
+            // BoxHoster
             // 
-            this.HosterBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.HosterBox.FormattingEnabled = true;
-            this.HosterBox.Location = new System.Drawing.Point(15, 68);
-            this.HosterBox.Name = "HosterBox";
-            this.HosterBox.Size = new System.Drawing.Size(121, 21);
-            this.HosterBox.TabIndex = 0;
-            this.HosterBox.SelectedIndexChanged += new System.EventHandler(this.HosterBox_SelectedIndexChanged);
+            this.BoxHoster.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.BoxHoster.FormattingEnabled = true;
+            this.BoxHoster.Location = new System.Drawing.Point(15, 68);
+            this.BoxHoster.Name = "BoxHoster";
+            this.BoxHoster.Size = new System.Drawing.Size(121, 21);
+            this.BoxHoster.TabIndex = 0;
+            this.BoxHoster.SelectedIndexChanged += new System.EventHandler(this.HosterBox_SelectedIndexChanged);
             // 
             // label1
             // 
@@ -57,14 +63,15 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Hoster";
             // 
-            // MirrorBox
+            // BoxMirror
             // 
-            this.MirrorBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.MirrorBox.FormattingEnabled = true;
-            this.MirrorBox.Location = new System.Drawing.Point(154, 68);
-            this.MirrorBox.Name = "MirrorBox";
-            this.MirrorBox.Size = new System.Drawing.Size(121, 21);
-            this.MirrorBox.TabIndex = 2;
+            this.BoxMirror.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.BoxMirror.FormattingEnabled = true;
+            this.BoxMirror.Location = new System.Drawing.Point(154, 68);
+            this.BoxMirror.Name = "BoxMirror";
+            this.BoxMirror.Size = new System.Drawing.Size(121, 21);
+            this.BoxMirror.TabIndex = 2;
+            this.BoxMirror.SelectedIndexChanged += new System.EventHandler(this.BoxMirror_SelectedIndexChanged);
             // 
             // label2
             // 
@@ -117,19 +124,84 @@
             this.BoxEpisode.TabIndex = 7;
             this.BoxEpisode.SelectedIndexChanged += new System.EventHandler(this.BoxEpisode_SelectedIndexChanged);
             // 
+            // ButtonOk
+            // 
+            this.ButtonOk.Location = new System.Drawing.Point(12, 197);
+            this.ButtonOk.Name = "ButtonOk";
+            this.ButtonOk.Size = new System.Drawing.Size(75, 23);
+            this.ButtonOk.TabIndex = 8;
+            this.ButtonOk.Text = "Ok";
+            this.ButtonOk.UseVisualStyleBackColor = true;
+            this.ButtonOk.Click += new System.EventHandler(this.ButtonOk_Click);
+            // 
+            // LabelPart
+            // 
+            this.LabelPart.AutoSize = true;
+            this.LabelPart.Enabled = false;
+            this.LabelPart.Location = new System.Drawing.Point(15, 97);
+            this.LabelPart.Name = "LabelPart";
+            this.LabelPart.Size = new System.Drawing.Size(26, 13);
+            this.LabelPart.TabIndex = 10;
+            this.LabelPart.Text = "Part";
+            // 
+            // BoxPart
+            // 
+            this.BoxPart.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.BoxPart.Enabled = false;
+            this.BoxPart.FormattingEnabled = true;
+            this.BoxPart.Location = new System.Drawing.Point(15, 113);
+            this.BoxPart.Name = "BoxPart";
+            this.BoxPart.Size = new System.Drawing.Size(121, 21);
+            this.BoxPart.TabIndex = 9;
+            this.BoxPart.SelectedIndexChanged += new System.EventHandler(this.BoxPart_SelectedIndexChanged);
+            // 
+            // LabelHosterLink
+            // 
+            this.LabelHosterLink.AutoSize = true;
+            this.LabelHosterLink.Location = new System.Drawing.Point(18, 146);
+            this.LabelHosterLink.Name = "LabelHosterLink";
+            this.LabelHosterLink.Size = new System.Drawing.Size(35, 13);
+            this.LabelHosterLink.TabIndex = 11;
+            this.LabelHosterLink.Text = "label3";
+            // 
+            // LabelPlay
+            // 
+            this.LabelPlay.AutoSize = true;
+            this.LabelPlay.Location = new System.Drawing.Point(18, 169);
+            this.LabelPlay.Name = "LabelPlay";
+            this.LabelPlay.Size = new System.Drawing.Size(35, 13);
+            this.LabelPlay.TabIndex = 12;
+            this.LabelPlay.Text = "label3";
+            // 
+            // ButtonPlay
+            // 
+            this.ButtonPlay.Location = new System.Drawing.Point(93, 197);
+            this.ButtonPlay.Name = "ButtonPlay";
+            this.ButtonPlay.Size = new System.Drawing.Size(75, 23);
+            this.ButtonPlay.TabIndex = 13;
+            this.ButtonPlay.Text = "Play";
+            this.ButtonPlay.UseVisualStyleBackColor = true;
+            this.ButtonPlay.Click += new System.EventHandler(this.ButtonPlay_Click);
+            // 
             // StreamInfo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(286, 108);
+            this.ClientSize = new System.Drawing.Size(656, 310);
+            this.Controls.Add(this.ButtonPlay);
+            this.Controls.Add(this.LabelPlay);
+            this.Controls.Add(this.LabelHosterLink);
+            this.Controls.Add(this.LabelPart);
+            this.Controls.Add(this.BoxPart);
+            this.Controls.Add(this.ButtonOk);
             this.Controls.Add(this.BoxEpisode);
             this.Controls.Add(this.BoxSeason);
             this.Controls.Add(this.LabelEpisode);
             this.Controls.Add(this.LabelSeason);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.MirrorBox);
+            this.Controls.Add(this.BoxMirror);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.HosterBox);
+            this.Controls.Add(this.BoxHoster);
             this.Name = "StreamInfo";
             this.Text = "StreamInfo";
             this.ResumeLayout(false);
@@ -139,13 +211,19 @@
 
         #endregion
 
-        private System.Windows.Forms.ComboBox HosterBox;
+        private System.Windows.Forms.ComboBox BoxHoster;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox MirrorBox;
+        private System.Windows.Forms.ComboBox BoxMirror;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label LabelSeason;
         private System.Windows.Forms.Label LabelEpisode;
         private System.Windows.Forms.ComboBox BoxSeason;
         private System.Windows.Forms.ComboBox BoxEpisode;
+        private System.Windows.Forms.Button ButtonOk;
+        private System.Windows.Forms.Label LabelPart;
+        private System.Windows.Forms.ComboBox BoxPart;
+        private System.Windows.Forms.Label LabelHosterLink;
+        private System.Windows.Forms.Label LabelPlay;
+        private System.Windows.Forms.Button ButtonPlay;
     }
 }
--- XBMCStreamingBrowser/StreamInfo.cs
+++ XBMCStreamingBrowser/StreamInfo.cs
@@ -9,6 +9,7 @@ using System.Windows.Forms;
 using System.Net;
 using System.Text.RegularExpressions;
 using System.Collections.Specialized;
+using XBMCStreamingBrowser.Plugin.Generic;
 
 namespace XBMCStreamingBrowser
 {
@@ -21,6 +22,7 @@ namespace XBMCStreamingBrowser
         private string _seriesUrl = "";
         private string _mainString;
         WebClient wc = new WebClient();
+        private string _hosterLink = "";
 
         public StreamInfo(string url)
         {
@@ -36,6 +38,93 @@ namespace XBMCStreamingBrowser
         {
             string url = String.Format("http://kinox.to/aGET/MirrorByEpisode/{0}&Season={1}&Episode={2}", _seriesUrl, season, episode);
             setMirror(url, true);
+        }
+
+        private void setHosterLink()
+        {
+            Hoster hosterObj = (Hoster)BoxHoster.SelectedItem;
+            string hoster = hosterObj.id.ToString();
+            string mirror = BoxMirror.SelectedItem.ToString();
+
+            string url = String.Format("http://kinox.to/aGET/Mirror/{0}&Hoster={1}&Mirror={2}", _mainString, hoster, mirror);
+
+            if (BoxSeason.SelectedItem != null && BoxSeason.Enabled == true)
+            {
+                string season = BoxSeason.SelectedItem.ToString();
+                url += String.Format("&Season={0}", season);
+            }
+
+            if (BoxEpisode.SelectedItem != null && BoxEpisode.Enabled == true)
+            {
+                string episode = BoxEpisode.SelectedItem.ToString();
+                url += String.Format("&Episode={0}", episode);
+            }
+
+            if (BoxPart.SelectedItem != null && BoxPart.Enabled == true)
+            {
+                string part = BoxPart.SelectedItem.ToString();
+                url += String.Format("&Part={0}", part);
+            }
+
+            string result = wc.DownloadString(url);
+            string resultString = Regex.Match(result, @"<a href=\\""(.*?)""", RegexOptions.Singleline).Groups[1].Value;
+
+            _hosterLink = System.Text.RegularExpressions.Regex.Unescape(resultString);
+            LabelHosterLink.Text = _hosterLink;
+
+
+        }
+
+        private void setPart()
+        {
+
+            Hoster hosterObj = (Hoster)BoxHoster.SelectedItem;
+            string hoster = hosterObj.id.ToString();
+            string mirror = BoxMirror.SelectedItem.ToString();
+
+            string url = String.Format("http://kinox.to/aGET/Mirror/{0}&Hoster={1}&Mirror={2}", _mainString, hoster, mirror);
+
+            if (BoxSeason.SelectedItem != null && BoxEpisode.SelectedItem != null)
+            {
+                string season = BoxSeason.SelectedItem.ToString();
+                string episode = BoxEpisode.SelectedItem.ToString();
+
+                url = url + String.Format("&Season={0}&Episode={1}", season, episode);
+            }
+
+            string result = wc.DownloadString(url);
+
+            Regex regexObj = new Regex(@"Part (.*?)<", RegexOptions.Compiled);
+            Match matchResults = regexObj.Match(result);
+
+            int maxPart = 0;
+
+            while (matchResults.Success)
+            {
+                maxPart = int.Parse(matchResults.Groups[1].Value);
+                matchResults = matchResults.NextMatch();
+            }
+
+            if (maxPart != 0)
+            {
+                BoxPart.Enabled = true;
+                LabelPart.Enabled = true;
+
+                BoxPart.Items.Clear();
+
+                for (int i = 1; i <= maxPart; i++)
+                {
+                    BoxPart.Items.Add(i);
+                }
+
+                BoxPart.SelectedIndex = 0;
+
+            }
+            else
+            {
+                BoxPart.Enabled = false;
+                LabelPart.Enabled = false;
+            }
         }
 
         private void setMirror(string url, bool isSeries=false)
@@ -77,8 +166,8 @@ namespace XBMCStreamingBrowser
                 matchResults = matchResults.NextMatch();
             }
 
-            HosterBox.DataSource = hosterList;
-            HosterBox.DisplayMember = "name";
+            BoxHoster.DataSource = hosterList;
+            BoxHoster.DisplayMember = "name";
         }
 
         private List<KeyValuePair<int, int>> getSeasonList()
@@ -116,17 +205,17 @@ namespace XBMCStreamingBrowser
 
         private void HosterBox_SelectedIndexChanged(object sender, EventArgs e)
         {
-            MirrorBox.Items.Clear();
-            var selectedHoster = (Hoster)HosterBox.SelectedItem;
+            BoxMirror.Items.Clear();
+            var selectedHoster = (Hoster)BoxHoster.SelectedItem;
 
             for (int i = 1; i <= selectedHoster.mirrors; i++)
             {
-                MirrorBox.Items.Add(i.ToString());
+                BoxMirror.Items.Add(i.ToString());
             }
 
             var random = new Random();
-            int rnd = random.Next(0, MirrorBox.Items.Count - 1);
-            MirrorBox.SelectedIndex = rnd;
+            int rnd = random.Next(0, BoxMirror.Items.Count - 1);
+            BoxMirror.SelectedIndex = rnd;
         }
 
         private void BoxSeason_SelectedIndexChanged(object sender, EventArgs e)
@@ -148,6 +237,42 @@ namespace XBMCStreamingBrowser
         private void BoxEpisode_SelectedIndexChanged(object sender, EventArgs e)
         {
             setMirrorByEpisode(BoxSeason.SelectedItem.ToString(), BoxEpisode.SelectedItem.ToString());
+        }
+
+        private void ButtonOk_Click(object sender, EventArgs e)
+        {
+            var resolved = PluginHandler.Resolve(_hosterLink);
+            if (!String.IsNullOrEmpty(resolved))
+            {
+                LabelPlay.Text = resolved;
+            }
+            else
+            {
+                MessageBox.Show("Kein Plugin gefunden");
+            }
+        }
+
+        private void BoxMirror_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            setHosterLink();
+        }
+
+        private void BoxPart_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            setHosterLink();
+        }
+
+        private void ButtonPlay_Click(object sender, EventArgs e)
+        {
+            var test = new JsonRpcClient();
+            using (WebClient client = new WebClient())
+            {
+                client.Headers.Add("Content-Type", "application/json");
+
+                var json1 = @"{""jsonrpc"": ""2.0"", ""method"": ""Player.Open"", ""params"":{""item"":{""file"":"""+LabelPlay.Text+@"""}}, ""id"": 1}";
+
+                string response = client.UploadString("http://xbmc:xmbc@192.168.178.24/jsonrpc", json1);
+            }
         }
     }
 }
